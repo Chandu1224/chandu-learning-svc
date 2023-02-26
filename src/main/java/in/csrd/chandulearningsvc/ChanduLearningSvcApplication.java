309b5c2c3f8e3525f220230809ae2baa06064473
Chandu1224 <csr.devireddy1212@gmail.com>
2023-02-26 14:42:05 +0530

    added intial code

---
+++ src/main/java/in/csrd/chandulearningsvc/ChanduLearningSvcApplication.java
@@ -0,0 +1,13 @@
+package in.csrd.chandulearningsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanduLearningSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChanduLearningSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/csrd/chandulearningsvc/ChanduLearningSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package in.csrd.chandulearningsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanduLearningSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
